4de19c0907bc31c261afd68ab5b669815aaa5089
David McNett <nugget@flightaware.com>
2012-04-09 15:33:54 -0500

    Table definitions for logging and storage tables.

---
+++ db/schema.sql
@@ -0,0 +1,104 @@
+-- add a role for each bot which will use this database
+CREATE ROLE dbusername WITH LOGIN ENCRYPTED PASSWORD 'password';
+
+CREATE ROLE bots;
+GRANT bots TO dbusername;
+
+CREATE OR REPLACE FUNCTION onupdate_changed() RETURNS trigger AS $$
+	BEGIN
+		NEW.changed := (current_timestamp at time zone 'UTC');
+		RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TABLE songs (
+	song_id varchar NOT NULL,
+	added timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	changed timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	artist varchar,
+	song varchar,
+	album varchar,
+	genre varchar,
+	length integer,
+	mnid varchar,
+	coverart varchar,
+	md5 varchar,
+	labelid integer,
+	PRIMARY KEY(song_id)
+);
+GRANT SELECT,INSERT ON songs TO bots;
+CREATE TRIGGER onupdate BEFORE UPDATE ON songs FOR EACH ROW EXECUTE PROCEDURE onupdate_changed();
+
+CREATE TABLE users (
+	user_id varchar NOT NULL,
+	added timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	changed timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	nickname varchar NOT NULL,
+	password varchar,
+	owner boolean NOT NULL DEFAULT FALSE,
+	admin boolean NOT NULL DEFAULT FALSE,
+	trendsetter boolean NOT NULL DEFAULT FALSE,
+	PRIMARY KEY(user_id)
+);
+GRANT SELECT,INSERT,UPDATE ON users TO bots;
+CREATE TRIGGER onupdate BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE onupdate_changed();
+
+CREATE TABLE users_nicknames (
+	id SERIAL NOT NULL,
+	userid varchar NOT NULL REFERENCES users(user_id),
+	added timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	nickname varchar NOT NULL,
+	PRIMARY KEY(id)
+);
+GRANT SELECT,INSERT ON users_nicknames TO bots;
+GRANT ALL ON users_nicknames_id_seq TO bots;
+
+CREATE TABLE songlog (
+	id serial NOT NULL,
+	ts timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	song_id varchar NOT NULL,
+	room_id varchar NOT NULL,
+	user_id varchar NOT NULL,
+	stats_djcount integer,
+	stats_listeners integer,
+	stats_djs varchar,
+	PRIMARY KEY(id)
+);
+GRANT SELECT,INSERT,UPDATE ON songlog TO bots;
+GRANT ALL ON songlog_id_seq TO bots;
+
+CREATE TABLE snaglog (
+	id serial NOT NULL,
+	ts timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	play_id integer NOT NULL REFERENCES songlog(id),
+	user_id varchar NOT NULL,
+	PRIMARY KEY(id)
+);
+GRANT SELECT,INSERT ON snaglog TO bots;
+GRANT ALL ON snaglog_id_seq TO bots;
+
+CREATE TABLE votelog (
+	id serial NOT NULL,
+	ts timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	play_id integer NOT NULL REFERENCES songlog(id),
+	user_id varchar,
+	vote integer NOT NULL DEFAULT 1,
+	PRIMARY KEY(id)
+);
+GRANT SELECT,INSERT ON votelog TO bots;
+GRANT ALL ON votelog_id_seq TO bots;
+
+CREATE TABLE queue (
+	id serial NOT NULL,
+	added timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	changed timestamp(0) without time zone NOT NULL DEFAULT (current_timestamp at time zone 'utc'),
+	deleted timestamp(0) without time zone,
+	sequence integer NOT NULL,
+	user_id varchar NOT NULL,
+	song_id varchar NOT NULL,
+	PRIMARY KEY(id)
+);
+CREATE TRIGGER onupdate BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE onupdate_changed();
+
+GRANT SELECT,INSERT ON votelog TO bots;
+GRANT ALL ON votelog_id_seq TO bots;
