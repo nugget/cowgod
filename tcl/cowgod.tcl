93588371c75d94781aa068b5e010bac07cba44d4
David McNett <nugget@flightaware.com>
2013-06-07 17:22:25 -0500

    Add standard buttonbar to the top of each page.

--- tcl/cowgod.tcl
+++ tcl/cowgod.tcl
@@ -56,6 +56,22 @@ namespace eval ::cowgod {
 		}
 		return "Unknown DJ"
 	}
+
+	proc buttonbar {} {
+		webout [el_open div -class row]
+		webout [el_open div -class span12]
+		webout "[el_open div -class btn-group]"
+		webout "[el_open button -class "btn"]<a href=\"/cowgod/\">[el_oc img -style "height: 1em;" -src [::cowgod::avatar_href 15 headfront]]</a>[el_close button]"
+		foreach {href title} {djstats "DJ Stats" recent "Recent Plays" bagel "CBDIA"} {
+			webout "[el_open button -class "btn"]<a href=\"$href\">$title</a></button>"
+		}
+		webout "[el_close div]"
+		webout "[el_open form -class "navbar-search pull-right" -action "/cowgod/search"]"
+		webout "[el_oc input -name "q" -type text -class "search-query" -placeholder "Search"]"
+		webout "[el_close form]"
+		webout [el_close div]
+		webout [el_close div]
+	}
 }
 
 package provide cowgod 1.0
