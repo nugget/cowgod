657d05c8e26823e687421079b33069415e4d900e
David McNett <nugget@flightaware.com>
2013-06-07 18:25:42 -0500

    Steal avatars directly from TT

--- steal_avatars.tcl
+++ steal_avatars.tcl
@@ -6,13 +6,14 @@ package require tls
 proc main {} {
 	set writepath [file join "." "public_html" "avatars"]
 	set start 1
-	set end 500
+	set end 1200
 
 	::http::register https 443 ::tls::socket
 
 	for {set i $start} {$i <= $end} {incr i} {
 		foreach image {fullfront headfront fullback headback} {
 			set url "https://s3.amazonaws.com/static.turntable.fm/roommanager_assets/avatars/${i}/${image}.png"
+			set url "https://turntable.fm/roommanager_assets/avatars/${i}/${image}.png"
 			set filename "[file join $writepath "${image}_[format "%03d" $i].png"]"
 			# puts "$url -> $filename"
 
