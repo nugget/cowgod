d4467f553968da17b6a0a06eb1ce942e5e348935
Pinkeeee <swim_24_7@hotmail.com>
2013-08-17 18:29:23 -0500

    Update pinkeeee.js

--- pinkeeee.js
+++ pinkeeee.js
@@ -1548,7 +1548,7 @@ bot.on('speak', function (data) {
 	}
 	
 	if (settings.userid == '51fc4bb6eb35c104c18698d2') {
-		if (data.text.toLowerCase().indexOf('@Pink's Mini Me') != -1) {
+		if (data.text.toLowerCase().indexOf('@Pink\'s Mini Me') != -1) {
 			logger('= '+id_to_name(data.senderid)+' said my name');
 			
 		if (data.user[0].userid == '51813658aaa5cd3df7b79831') {
