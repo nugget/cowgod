7b68f262e5377caced311835af7879bf6f3450af
Pinkeeee <swim_24_7@hotmail.com>
2013-08-17 20:34:16 -0500

    @pink\'s mini me*
    fixed typo....was missing an m

--- pinkeeee.js
+++ pinkeeee.js
@@ -1548,7 +1548,7 @@ bot.on('speak', function (data) {
 	}
 	
 	if (settings.userid == '51fc4bb6eb35c104c18698d2') {
-		if (data.text.toLowerCase().indexOf('@pink\'s mini e') != -1) {
+		if (data.text.toLowerCase().indexOf('@pink\'s mini me') != -1) {
 			logger('= '+id_to_name(data.senderid)+' said my name');
 			
 			if (data.userid == '51813658aaa5cd3df7b79831') {
