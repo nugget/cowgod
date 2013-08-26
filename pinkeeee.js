2d856b2ade496a56b4eeefe3d50e18c8fbfcd470
Pinkeeee <swim_24_7@hotmail.com>
2013-08-26 17:30:44 -0500

    Update Mini me's name to miniature pink

--- pinkeeee.js
+++ pinkeeee.js
@@ -1567,7 +1567,7 @@ bot.on('speak', function (data) {
 	}
 	
 	if (settings.userid == '51fc4bb6eb35c104c18698d2') {
-		if (data.text.toLowerCase().indexOf('@pink\'s mini me') != -1) {
+		if (data.text.toLowerCase().indexOf('@miniature pink') != -1) {
 			logger('= '+id_to_name(data.senderid)+' said my name');
 			say_command('/usr/games/fortune minimetexts');
 		}
