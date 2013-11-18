3a10631799a21e8da1dde1e24ded3bd15f143d8c
Pinkeeee <swim_24_7@hotmail.com>
2013-11-17 20:45:25 -0600

    Que Adjustments
    Add ignore around que section to ignore if mini me says it
    and change queue to be before que and to return if item is completed

--- pinkeeee.js
+++ pinkeeee.js
@@ -1628,23 +1628,23 @@ bot.on('speak', function (data) {
 			say_command('/usr/games/fortune minimetexts');
 		}
 	}
-	 
+	 if (data.userid == settings.userid || data.userid == '51fc4bb6eb35c104c18698d2'){ {
+		// logger('- ignoring self-snag');
+		// this is me!  ignore it
+		return;}
 	 if (data.text.toLowerCase().indexOf('q+') != -1)
 	 {say('You have been added to the Cutest Que EVER!') ;
 			}
 	if (data.text.toLowerCase().indexOf('/q') != -1)
 	 {say('You have been added to the Cutest Que EVER!');
 			}
-	if (data.text.toLowerCase().indexOf('que') != -1){
-	if (data.userid == settings.userid || data.userid == '51fc4bb6eb35c104c18698d2') {
-		// logger('- ignoring self-snag');
-		// this is me!  ignore it
-		return;}
-	 say('You have been added to the Cutest Que EVER!') ;
-			}
 	if (data.text.toLowerCase().indexOf('queue') != -1)
 	 {say('we will be with you as soon as possible, please take a number....now serving #69');
+			{return}}
+	if (data.text.toLowerCase().indexOf('que') != -1){
+	 say('You have been added to the Cutest Que EVER!') ;
 			}
+	 }
 			
 			
 	if (settings.userid == '51fc4bb6eb35c104c18698d2') {
