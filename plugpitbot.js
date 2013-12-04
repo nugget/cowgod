e059efb660e2ca127fb4f784b8d11aca47de5b20
David McNett <nugget@flightaware.com>
2013-12-04 13:14:09 -0600

    Move common cowgod code into a separate js file

--- plugpitbot.js
+++ plugpitbot.js
@@ -7,6 +7,8 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 
+var cowgod = require('./cowgod.js');
+
 if (typeof argv.nick === 'undefined') {
 	console.log('Need a -nick name!');
 	process.exit(1);
@@ -14,7 +16,7 @@ if (typeof argv.nick === 'undefined') {
 	var myname = argv.nick;
 	var settings = require('./settings_'+myname+'.js');
 }       
-logger('! My Name Is '+myname+' headed for '+settings.plug_room);
+cowgod.logger('! My Name Is '+myname+' headed for '+settings.plug_room);
 
 var usernames = new Object();
 
@@ -23,19 +25,36 @@ var UPDATECODE = 'fe940c';
 
 var log_tsv  = fs.createWriteStream(settings.log_tsv,  {'flags': 'a'});
 
+var nugget = {
+	//moo: function() {
+	//	console.log('Moo!');
+	//};
+
+	log: function() {
+		cowgod.logger('raw nugget.log');
+		for (var i = 0; i < arguments.length; i++) {
+			console.log(arguments[i]);
+		}
+	}
+}
+
 // var bot = new PlugAPI(settings.plug_auth);
 // bot.connect(settings.plug_room);
 //
 
 var bot = new PlugAPI(settings.plug_auth, UPDATECODE);
-logger('connecting to '+settings.plug_room);
+cowgod.logger('doing that logging thing, whatever the fuck that is');
+bot.setLogObject(nugget);
+cowgod.logger('connecting to '+settings.plug_room);
 bot.connect(settings.plug_room);
 
 bot.on('roomJoin', function(data) {
-	logger('roomJoin');
+	cowgod.logger('roomJoin');
 	logger_tsv([ 'event','roomJoin','nickname',data.user.profile.username,'plug_user_id',data.user.profile.id,'djPoints',data.user.profile.djPoints,'fans',data.user.profile.fans,'listenerPoints',data.user.profile.listenerPoints,'avatarID',data.user.profile.avatarid ]);
 	util.log(util.inspect(data));
 	remember_user(data.user.profile.id,data.user.profile.username);
+	process_waitlist();
+
 });
 
 bot.on('chat', function(data) {
@@ -49,12 +68,12 @@ bot.on('emote', function(data) {
 });
 
 bot.on('close', function(data) {
-	logger('close');
+	cowgod.logger('close');
 	util.log(util.inspect(data));
 });
 
 bot.on('error', function(data) {
-	logger('error');
+	cowgod.logger('error');
 	util.log(util.inspect(data));
 });
 
@@ -68,8 +87,8 @@ bot.on('userLeave', function(data) {
 });
 
 bot.on('djUpdate', function(data) {
-	logger('djUpdate');
-	util.log(util.inspect(data));
+	log_djupdate(data);
+	process_waitlist();
 });
 
 bot.on('curateUpdate', function(data) {
@@ -82,7 +101,7 @@ bot.on('voteUpdate', function(data) {
 });
 
 bot.on('userUpdate', function(data) {
-	logger('userUpdate');
+	cowgod.logger('userUpdate');
 	util.log(util.inspect(data));
 });
 
@@ -91,31 +110,19 @@ bot.on('djAdvance', function(data) {
 	if (data.media.author !== 'undefined') {
 		lag_vote();
 	}
+	process_waitlist();
 });
 
 function do_vote (vote) {
 	// bot.chat('Woot!');
-	logger(' I am wooting');
+	cowgod.logger(' I am wooting');
 	bot.woot();
 }
 
 function lag_vote (vote) {
 	waitms = parseInt(Math.random() * 20000)+500;
-	logger('- will vote '+vote+' in '+waitms+' ms');
+	cowgod.logger('- will vote '+vote+' in '+waitms+' ms');
 	setTimeout(function(){ do_vote(vote); }, waitms);
-}
-
-function logger(buf) {
-	var d=new Date();
-	var hh=d.getHours();
-	var mm=d.getMinutes();
-	if(mm < 10) {
-		mm = '0'+mm;
-	}
-	if(hh < 10) {
-		hh = '0'+hh;
-	}
-	console.log('['+hh+':'+mm+'] '+buf);
 }
 
 function logger_tsv(larray) {
@@ -132,11 +139,11 @@ function logger_tsv(larray) {
 
 function log_chat(data) {
 	if (data.type == 'message') {
-		logger('<'+data.from+'> '+data.message);
+		cowgod.logger('<'+data.from+'> '+data.message);
 	} else if (data.type == 'emote') {
-		logger('* '+data.from+' '+data.message);
+		cowgod.logger('* '+data.from+' '+data.message);
 	} else {
-		logger('chat (unknown type)');
+		cowgod.logger('chat (unknown type)');
 		util.log(util.inspect(data));
 	}
 	logger_tsv([ 'event','chat','nickname',data.from,'room',data.room,'plug_user_id',data.fromID,'message',data.message,'type',data.type ]);
@@ -144,39 +151,52 @@ function log_chat(data) {
 
 function log_vote(data) {
 	if (data.vote == 1) {
-		logger(id_to_name(data.id)+' wooted');
+		cowgod.logger(id_to_name(data.id)+' wooted');
 	} else {
-		logger('vote (unknown type)');
+		cowgod.logger('vote (unknown type)');
 		util.log(util.inspect(data));
 	}
 	logger_tsv([ 'event','vote','vote',data.vote,'plug_user_id',data.id ]);
 }
 
 function log_join(data) {
-	logger(data.username+' joined the room');
+	cowgod.logger(data.username+' joined the room');
 	logger_tsv([ 'event','join','nickname',data.username,'plug_user_id',data.id,'status',data.status,'fans',data.fans,'listenerPoints',data.listenerPoints,'avatarID',data.avatarID,'djPoints',data.djPoints,'permission',data.permission ]);
 }
 
 function log_part(data) {
-	logger(id_to_name(data.id)+' left the room');
+	cowgod.logger(id_to_name(data.id)+' left the room');
 	logger_tsv([ 'event','part','plug_user_id',data.id ]);
 }
 
 function log_curate(data) {
-	logger(id_to_name(data.id)+' snagged this song');
+	cowgod.logger(id_to_name(data.id)+' snagged this song');
 	logger_tsv([ 'event','snag','plug_user_id',data.id ]);
 }
 
 function log_play(data) {
-	logger(id_to_name(data.currentDJ)+' is playing '+data.media.title+' by '+data.media.author);
+	cowgod.logger(id_to_name(data.currentDJ)+' is playing '+data.media.title+' by '+data.media.author);
 	if (data.media.author !== 'undefined') {
 		logger_tsv( [ 'event','djAdvance','plug_user_id',data.currentDJ,'playlistID',data.playlistID,'song',data.media.author,'title',data.media.title,'duration',data.media.duration,'media_id',data.media.id,'media_cid',data.media.cid,'media_format',data.media.format ]);
 	}
 }
 
+function log_djupdate(data) {
+	cowgod.logger('djUpdate:');
+	util.log(util.inspect(data));
+	cowgod.logger('--');
+	util.log(util.inspect(data.djs));
+	cowgod.logger('--');
+	for (var u in data.djs) {
+		cowgod.logger('logging a u.user '+u);
+		util.log(util.inspect(data.djs[u]));
+	}
+}
+
+
 function remember_user(id,name) {
 	if (typeof usernames[id] === 'undefined') {
-		logger('- remembering that '+name+' is user_id '+id);
+		cowgod.logger('- remembering that '+name+' is user_id '+id);
 		usernames[id] = name;
 	}
 }
@@ -197,4 +217,12 @@ function name_to_id (username) {
 		}
 	}
 	return;
+}
+
+function process_waitlist() {
+	cowgod.logger('calling getWaitList');
+	bot.getWaitList(function(data) {
+		cowgod.logger('I gotWaitList');
+		util.log(util.inspect(data));
+	});
 }
