340f2a85e2f0ca975e3703b6c7ada3cc9ba4deba
David McNett <nugget@flightaware.com>
2014-09-14 19:20:51 -0500

    Doesn't work.

--- sample.js
+++ sample.js
@@ -1,43 +1,42 @@
 #!/usr/bin/nodejs
 
-var PlugBotAPI = require('plugbotapi');
+var PlugBotAPI  = require('./plugbotapi');
 
-PlugBotAPI.getAuth({
-  username: 'flightaware', // twitter username
-  password: 'WFz4wed7zJ' // twitter password
-}, function(err, auth) {
+var creds = {
+	email: 'cowgod@macnugget.org',
+	password: 'BivvUm9Xh(t)n2JW'
+};
 
-  console.log('auth code is '+auth);
+console.log('connecting to plug');
+var plugbotapi = new PlugBotAPI(creds);
+var room = 'pit-of-no-shame';
 
-  var plugbotapi = new PlugBotAPI(auth);
-  var room = 'pit-of-no-shame';
+console.log('joinging community');
+plugbotapi.connect(room);
 
-  plugbotapi.connect(room);
+plugbotapi.on('roomJoin', function() {
+	console.log("Connected!");
 
-  plugbotapi.on('roomJoin', function() {
-    console.log("Connected!");
+	plugbotapi.chat('Hello World');
 
-    plugbotapi.chat('Hello World');
-
-    plugbotapi.getUsers(function(users) {
+	plugbotapi.getUsers(function(users) {
       console.log("Number of users in the room: " + users.length);
     });
 
     plugbotapi.hasPermission('52a648c496fba57878e8f809', 'API.ROLE.NONE', function(result) {
       console.log("permission: ", result);
     });
-  });
+});
 
-  // A few sample events
-  plugbotapi.on('chat', function(data) {
-    console.log("got chat: ", data);
-  });
+// A few sample events
+plugbotapi.on('chat', function(data) {
+	console.log("got chat: ", data);
+});
 
-  plugbotapi.on('djAdvance', function(data) {
-    console.log("dj advance: ", data);
-  });
+plugbotapi.on('djAdvance', function(data) {
+	console.log("dj advance: ", data);
+});
 
-  plugbotapi.on('voteUpdate', function(data) {
-    console.log("vote update: ", data);
-  });
+plugbotapi.on('voteUpdate', function(data) {
+	console.log("vote update: ", data);
 });
