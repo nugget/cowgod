bec09856f25d8a3ec35e5a7a48755a950b13a47f
David McNett <nugget@flightaware.com>
2014-09-26 10:55:15 -0500

    Now the variables work.

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 rm -f /tmp/*.dmp
 
+SECS=300
+
 while [ 1 ]; do
 	./plugpitbot.js --nick $1
-	echo "waiting 60 seconds to restart"
-	sleep 60
+	echo "waiting $SECS seconds to restart"
+	sleep $SECS
 done
